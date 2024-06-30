e512b3de391c68b4e227db862be1006baf94c7aa
Non T <117605943+Ratanon691@users.noreply.github.com>
2024-06-30 10:38:36 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-// write code here
+let salaries = {
+  John: 100,
+  Ann: 160,
+  Pete: 130
+};
+let sum1 = Object.keys(salaries).reduce((acc, key) => acc + salaries[key], 0)
+let sum2 = Object.value(salaries).reduce((acc, value) => acc + value, 0)
+let sum3 = Object.entries(salaries).reduce((acc, [key, value]) => acc + value, 0)
+console.log(sum1) 
+console.log(sum2) 
+console.log(sum3) 
